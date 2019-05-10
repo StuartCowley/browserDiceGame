1263e133c4e3a4f39fd157112ab4a0faa474a7fe
StuartCowley <codenation18@CodeNation18s-MacBook-Pro.local>
2019-05-10 17:42:23 +0100

    upgraded to 2 player mode & added more CSS

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-let score = 0;
+// Initialise the game
 
-document.getElementById("score").innerHTML = "Current total: " + score;
+let scoreboard = {
+  player1: 0,
+  player2: 0,
+  player1Name: "Player 1",
+  player2Name: "Player 2"
+};
+let player = "player1";
+Player1Name.className = "blinkEffect";
 
+document.getElementById("player1Score").innerHTML =
+  "Current Total: " + scoreboard.player1;
+document.getElementById("player2Score").innerHTML =
+  "Current Total: " + scoreboard.player2;
+
+// Button functions
 document.getElementById("play").addEventListener("click", function() {
-  DiceGame();
+  onClick();
 });
 
 document.getElementById("restart").addEventListener("click", function() {
-  score = 0;
-  document.getElementById("score").innerHTML = "Current total: " + score;
+  scoreboard.player1 = 0;
+  scoreboard.player2 = 0;
+  document.getElementById("player1Score").innerHTML =
+    "Current Total: " + scoreboard.player1;
+  document.getElementById("player2Score").innerHTML =
+    "Current Total: " + scoreboard.player2;
   document.getElementById("message").innerHTML = "";
+  player = "player1";
+  Player2Name.className = "";
+  Player1Name.className = "blinkEffect";
 });
-
-const roll = () => {
-  let result = Math.floor(Math.random() * 6) + 1;
-  return result;
-};
 
 const checkScore = score => {
   if (score >= 20) {
@@ -24,46 +39,74 @@ const checkScore = score => {
   }
 };
 
-const DiceGame = () => {
+const roll = () => {
+  let result = Math.floor(Math.random() * 6) + 1;
+  return result;
+};
+
+const switchPlayer = () => {
+  if (player == "player1") {
+    player = "player2";
+    Player1Name.className = "";
+    Player2Name.className = "blinkEffect";
+  } else {
+    player = "player1";
+    Player2Name.className = "";
+    Player1Name.className = "blinkEffect";
+  }
+};
+
+const onClick = () => {
   if (document.getElementById("message").innerHTML == "") {
     turn = roll();
     switch (turn) {
       case 1:
         document.getElementById("dicePic").src = "../img/dice1.png";
-        score += turn;
+        scoreboard.player += turn;
         document.getElementById("message").innerHTML =
-          "Game over, you rolled a one!";
-        score = 0;
+          "You rolled a one, you lose!";
+        scoreboard.player = 0;
         break;
       case 2:
         document.getElementById("dicePic").src = "../img/dice2.png";
-        score += turn;
-        document.getElementById("score").innerHTML = "Current total: " + score;
-        checkScore(score);
+        scoreboard[player] += turn;
+        console.log(scoreboard[player]);
+        document.getElementById(player + "Score").innerHTML =
+          "Current Total: " + scoreboard[player];
+        checkScore(scoreboard[player]);
+        switchPlayer();
         break;
       case 3:
         document.getElementById("dicePic").src = "../img/dice3.png";
-        score += turn;
-        document.getElementById("score").innerHTML = "Current total: " + score;
-        checkScore(score);
+        scoreboard[player] += turn;
+        document.getElementById(player + "Score").innerHTML =
+          "Current Total: " + scoreboard[player];
+        checkScore(scoreboard[player]);
+        switchPlayer();
         break;
       case 4:
         document.getElementById("dicePic").src = "../img/dice4.png";
-        score += turn;
-        document.getElementById("score").innerHTML = "Current total: " + score;
-        checkScore(score);
+        scoreboard[player] += turn;
+        document.getElementById(player + "Score").innerHTML =
+          "Current Total: " + scoreboard[player];
+        checkScore(scoreboard[player]);
+        switchPlayer();
         break;
       case 5:
         document.getElementById("dicePic").src = "../img/dice5.png";
-        score += turn;
-        document.getElementById("score").innerHTML = "Current total: " + score;
-        checkScore(score);
+        scoreboard[player] += turn;
+        document.getElementById(player + "Score").innerHTML =
+          "Current Total: " + scoreboard[player];
+        checkScore(scoreboard[player]);
+        switchPlayer();
         break;
       case 6:
         document.getElementById("dicePic").src = "../img/dice6.png";
-        score += turn;
-        document.getElementById("score").innerHTML = "Current total: " + score;
-        checkScore(score);
+        scoreboard[player] += turn;
+        document.getElementById(player + "Score").innerHTML =
+          "Current Total: " + scoreboard[player];
+        checkScore(scoreboard[player]);
+        switchPlayer();
         break;
     }
   }
